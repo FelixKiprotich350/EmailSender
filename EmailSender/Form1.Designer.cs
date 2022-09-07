2fabc2beab10e77c83ffdcfe7f7b3477c9de95e5
felix kiprotich <felixkiprotich82@yahoo.com>
2022-09-07 11:13:49 +0300

    Add project files.

---
+++ EmailSender/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace EmailSender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.txtCC = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBody = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(279, 63);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "To Email";
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(95, 34);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(147, 20);
+            this.txtTo.TabIndex = 2;
+            // 
+            // txtCC
+            // 
+            this.txtCC.Location = new System.Drawing.Point(95, 70);
+            this.txtCC.Name = "txtCC";
+            this.txtCC.Size = new System.Drawing.Size(147, 20);
+            this.txtCC.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "CC TO";
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(95, 107);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(147, 20);
+            this.txtSubject.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Subject";
+            // 
+            // txtBody
+            // 
+            this.txtBody.Location = new System.Drawing.Point(95, 144);
+            this.txtBody.Multiline = true;
+            this.txtBody.Name = "txtBody";
+            this.txtBody.Size = new System.Drawing.Size(147, 85);
+            this.txtBody.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Subject";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 286);
+            this.Controls.Add(this.txtBody);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCC);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.TextBox txtCC;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBody;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ EmailSender/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmailSender
+{
+    public partial class Form1 : Form
+    {
+        MailMessage message;
+        SmtpClient smtp;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+
+            {
+
+                btnSend.Enabled = false; 
+
+                message = new MailMessage();
+
+                message.To.Add(txtTo.Text); 
+               // message.CC.Add(txtCC.Text);
+                message.Subject = txtSubject.Text;
+
+                message.From = new MailAddress("fkiprotich845@gmail.com");
+
+                message.Body = txtBody.Text;
+                //if (IsValidEmail(txtTo.Text))
+
+                //{
+
+                //    message.To.Add(txtTo.Text);
+
+                //}
+
+
+
+                //if (IsValidEmail(txtCC.Text))
+
+                //{
+
+                //    message.CC.Add(txtCC.Text);
+
+                //}
+
+                //message.Subject = txtSubject.Text;
+
+                //message.From = new MailAddress("deepak.sharma009@gmail.com");
+
+                //message.Body = txtBody.Text;
+
+                //if (lblAttachment.Text.Length > 0)
+
+                //{
+
+                //    if (System.IO.File.Exists(lblAttachment.Text))
+
+                //    {
+
+                //        message.Attachments.Add(new Attachment(lblAttachment.Text));
+
+                //    }
+
+                //}
+
+
+
+                // set smtp details
+
+                smtp = new SmtpClient("smtp.gmail.com");
+                smtp.UseDefaultCredentials = true;
+                smtp.Port = 587; 
+                smtp.Credentials = new NetworkCredential("fkiprotich845@gmail.com", "woinyickutgdicsj"); 
+                smtp.EnableSsl = true; 
+                smtp.SendAsync(message, message.Subject);
+
+                smtp.SendCompleted += new SendCompletedEventHandler(smtp_SendCompleted);
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                MessageBox.Show(ex.Message); 
+
+                btnSend.Enabled = true;
+
+            }
+        }
+        void smtp_SendCompleted(object sender, AsyncCompletedEventArgs e)
+
+        {
+
+            if (e.Cancelled == true)
+
+            {
+
+                MessageBox.Show("Email sending cancelled!");
+
+            }
+
+            else if (e.Error != null)
+
+            {
+
+                MessageBox.Show(e.Error.Message);
+
+            }
+
+            else
+
+            {
+
+                MessageBox.Show("Email sent sucessfully!");
+
+            }
+
+             
+
+            btnSend.Enabled = true;
+
+        }
+         
+        private void btnCancel_Click(object sender, EventArgs e)
+
+        {
+
+            smtp.SendAsyncCancel();
+
+            MessageBox.Show("Email sending cancelled!");
+
+        }
+        private void lnkAttachFile_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+
+        {
+
+        //    if (openFileDialog1.ShowDialog() != DialogResult.Cancel)
+
+        //    {
+
+        //        lblAttachment.Text = openFileDialog1.FileName;
+
+        //        lblAttachment.Visible = true;
+
+        //        lnkAttachFile.Visible = false;
+
+        //    }
+
+        }
+    }
+}
